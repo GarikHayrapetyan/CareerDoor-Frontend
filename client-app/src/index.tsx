ee83a1fec1552185d58b27416a0d049d6376e290
oybekabdulazizov <s17712@pjwstk.edu.pl>
2021-05-02 18:40:01 +0200

    imported react-datepicker

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import "./app/layout/style.css";
 import "react-calendar/dist/Calendar.css";
 import "semantic-ui-css/semantic.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import 'react-datepicker/dist/react-datepicker.css';
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import { store, StoreContext } from "./app/store/store";
