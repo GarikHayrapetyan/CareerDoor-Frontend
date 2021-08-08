5c7b3a5a219d340e112130f99c8bb18c3df9a456
Garik Hayrapetyan <garhay01@gmail.com>
2021-08-08 21:12:21 +0200

    Added user store and error displaying on the form

--- client-app/src/app/store/store.ts
+++ client-app/src/app/store/store.ts
@@ -1,15 +1,18 @@
 import { createContext, useContext } from 'react';
 import CommonStore from './commonStore';
 import GetTogetherStore from './getTogetherStore';
+import UserStore from './userStore';
 
 interface Store {
 	getTogetherStore: GetTogetherStore;
 	commonStore: CommonStore;
+	userStore: UserStore;
 }
 
 export const store: Store = {
 	getTogetherStore: new GetTogetherStore(),
-	commonStore: new CommonStore()
+	commonStore: new CommonStore(),
+	userStore: new UserStore()
 };
 
 export const StoreContext = createContext(store);
--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,23 +1,30 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
+import { observer } from "mobx-react-lite";
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import MyTextInput from "../../app/common/form/MyTextInput";
+import { useStore } from "../../app/store/store";
 
-export default function LoginForm() {
+export default observer(function LoginForm() {
+    const { userStore } = useStore();
     return (
         <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={values => console.log(values)}
-            >
+            initialValues={{ email: "", password: "", error: null }}
+            onSubmit={(values, { setErrors }) => userStore.login(values).catch(error =>
+                setErrors({ error: "Invalid email or password" }))}
+        >
 
-                {({handleSubmit})=>(
-                    <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
-                        <MyTextInput name="email" placeholder="Email"/>
-                        <MyTextInput name="password" placeholder="Password" type='password'/>
-                        <Button positive content="Login" type="submit" fluid/>
-                    </Form>
-                )}
+            {({ handleSubmit, isSubmitting, errors }) => (
+                <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
+                    <MyTextInput name="email" placeholder="Email" />
+                    <MyTextInput name="password" placeholder="Password" type='password' />
+                    <ErrorMessage 
+                        name="error" render={() => 
+                        <Label style={{ marginBottom: 10 }} basic color="red" content={errors.error} />}/>
+                    <Button loading={isSubmitting} positive content="Login" type="submit" fluid />
+                </Form>
+            )}
 
         </Formik>
     )
-}
+})
