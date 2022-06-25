c41c70a775339c7b383a29df22d7a1340d3ea0d1
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-06-25 15:34:11 +0200

    fix the HOST label and clean the header section

--- client-app/src/features/gettogethers/details/GetTogetherDetailedSidebar.tsx
+++ client-app/src/features/gettogethers/details/GetTogetherDetailedSidebar.tsx
@@ -23,22 +23,22 @@ function GetTogetherDetailedSidebar({ getTogether: { attendees, host } }: Props)
                 {attendees.length} {attendees.length === 1 ? 'Person' : 'People'} going
             </Segment>
             <Segment attached style={{ paddingRight: '0px' }}>
-                <List relaxed divided style={{ height: '190px', overflowY: 'scroll', marginRight: '14px' }}>
+                <List relaxed divided style={{ height: '190px', overflowY: 'scroll', marginRight: '14px', marginLeft: '6px' }}>
                     {attendees.map(attendee => (
                         <Item 
                         id='segmentt'
-                        style={{ position: 'relative', width: '95%', height: '33%', display: 'flex', alignItems: 'center' }} 
+                        style={{ position: 'relative', width: '92%', height: '33%', display: 'flex', alignItems: 'center' }} 
                         key={attendee.username}>
-                            {/* {attendee.username === host?.username &&
+                            {attendee.username === host?.username &&
                                 <Label
-                                    style={{ position: 'absolute', top: '8px',  }}
+                                    style={{ position: 'absolute', top: '8px', left: '100%' }}
                                     color='orange'
                                     ribbon='right'
                                     size='tiny'
                                 >
                                     Host
                                 </Label>
-                            } */}
+                            }
                             <Image size='tiny' circular src={attendee.image || '/assets/user.png'} style={{ width: '40px' }} />
                             <Item.Content verticalAlign='middle' style={{ width: '60%', flexWrap: 'wrap', wordWrap: 'break-word' }}>
                                 <Item.Header as='h3' style={{margin: 'auto 0 auto 5px', fontSize: '13px'}}>
@@ -48,14 +48,14 @@ function GetTogetherDetailedSidebar({ getTogether: { attendees, host } }: Props)
                                     <Item.Extra style={{ color: 'orange', marginLeft: '5px' }}>Following</Item.Extra>
                                 }
                             </Item.Content>
-                            {attendee.username === host?.username &&
+                            {/* {attendee.username === host?.username &&
                                 <Label
                                     style={{ position: 'absolute', float: 'right', right: '0'}}
                                     color='orange'
                                 >
                                     Host
                                 </Label>
-                            }
+                            } */}
                         </Item>
                     ))}
                 </List>
