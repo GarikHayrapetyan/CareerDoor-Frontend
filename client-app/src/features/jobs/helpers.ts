819c1528938c67043d885d9ccedd2416456a97b9
Muhammad Rizo <abduganeoff@gmail.com>
2022-07-12 17:04:13 +0200

    remove 24 hour option from posteddate

--- client-app/src/features/jobs/helpers.ts
+++ client-app/src/features/jobs/helpers.ts
@@ -1,8 +1,7 @@
 export const datePosted = [
 	{ key: 1, text: 'Any', value: 1 },
 	{ key: 2, text: 'Past Month', value: 2 },
-	{ key: 3, text: 'Past Week', value: 3 },
-	{ key: 4, text: 'Last 24 hours', value: 4 }
+	{ key: 3, text: 'Past Week', value: 3 }
 ];
 
 export const experienceLevel = [
